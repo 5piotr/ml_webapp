b13234a39cb1ab99c0976cc73ef171450f40d3d6
5piotr <piotr-pietka@wp.pl>
2024-03-28 08:47:57 +0000

    fixed apt spinner

--- app/api/static/js/apartment_price_estimator.js
+++ app/api/static/js/apartment_price_estimator.js
@@ -26,7 +26,7 @@ function onMapClick(e) {
 
 mymap.on('click', onMapClick);
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields and adding spinner
+// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
   'use strict';
   window.addEventListener('load', function() {
@@ -50,16 +50,30 @@ mymap.on('click', onMapClick);
 })();
 
 // spinner
-$('#form').on('change', function() {
-  $(".btn").click(function() {
-    // disable button
-    // $(this).prop("disabled", true);
-    // add spinner to button
-    $('#spinner').html(
-      `<div class="d-flex align-items-center">
-        <strong>Loading...</strong>
-        <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
-      </div>`
-    );
+document.addEventListener("DOMContentLoaded", function () {
+  var form = document.getElementById("form");
+  var spinner = document.getElementById("spinner");
+
+  form.addEventListener("submit", function (event) {
+
+    // Check if the form is valid
+    if (!form.checkValidity()) {
+      // If the form is invalid, do not proceed with the submission
+      return;
+    }
+
+    // Replace the submit button with the spinner
+    var submitButton = form.querySelector('button[type="submit"]');
+    submitButton.style.display = "none";
+    spinner.innerHTML = '<button class="btn btn-success" type="button" disabled><span class="spinner-border spinner-border-sm" aria-hidden="true"></span><span role="status"> Loading...</span></button>';
+
+    xhr.onerror = function () {
+      // Hide the spinner on error
+      spinner.innerHTML = '';
+      submitButton.style.display = "block";
+
+      console.error("Request failed");
+    };
+    xhr.send(formData);
   });
 });
