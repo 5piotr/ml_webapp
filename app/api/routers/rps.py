14d354b0818fec4605e963ec4b1a1a4b3a35b16a
5piotr <piotr-pietka@wp.pl>
2024-04-06 15:41:14 +0000

    added rps details

--- app/api/routers/rps.py
+++ app/api/routers/rps.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 router = APIRouter(
-    prefix='/rps',
+    prefix='/rps_gesture_recognition',
     tags=['rps gesture recognition']
 )
 
-@router.get('/estimate', include_in_schema=False)
-async def get_rps_pred():
-    return {'app stataus': 'currently not working'}
+templates = Jinja2Templates(directory='api/templates')
+
+@router.get('/details', response_class=HTMLResponse, include_in_schema=False)
+async def price_estimator(request: Request):
+
+    return templates.TemplateResponse('rps_cnn_details.html',
+                                      {'request': request})
